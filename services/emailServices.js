b29355ce0f48164868902b25faf574f6882b20d6
DavidAustine <davoace345@gmail.com>
2026-05-05 14:59:25 +0100

    otp

--- services/emailServices.js
+++ services/emailServices.js
@@ -1,26 +1,26 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-})
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 const sendOTPEmail = async (to, otp) => {
-    try {
-        const info = await transporter.sendMail({
-            from: `"Donora Blood" <${process.env.EMAIL_USER}>`,
-            to,
-            subject: "Your Password Reset OTP",
-            text: `Hello,\n\nYour OTP is: ${otp}\nIt expires in 10 minutes.\n\nIf you didn’t request this, ignore this email.\n\n-Blood Donation App Team`
-        })
-        console.log("Email sent:", info.response)
-    } catch (error) {
-        console.error("Email sending error:", error.message)
-        throw new Error("Could not send email")
-    }
-}
+  try {
+    const info = await transporter.sendMail({
+      from: `"Donora Blood" <${process.env.EMAIL_USER}>`,
+      to,
+      subject: "Your Password Reset OTP",
+      text: `Hello,\n\nYour OTP is: ${otp}\nIt expires in 10 minutes.\n\nIf you didn’t request this, ignore this email.\n\n-Blood Donation App Team`,
+    });
+    console.log("Email sent:", info.response);
+  } catch (error) {
+    console.error("FULL EMAIL ERROR:", error);
+    throw error;
+  }
+};
 
-module.exports = {sendOTPEmail}
+module.exports = { sendOTPEmail };
