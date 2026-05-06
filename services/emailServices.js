8f7fb29548cce11037d06849a9a73dcded587ec6
DavidAustine <davoace345@gmail.com>
2026-05-06 12:41:30 +0100

    otp fix

--- services/emailServices.js
+++ services/emailServices.js
@@ -2,13 +2,13 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // important
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000, // 10s
+  family: 4,
 });
 
 const sendOTPEmail = async (to, otp) => {
